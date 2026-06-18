916a80514c1ac5829dbbf113616ff1534f5f7019
bogdansaniuta <bogdansaniuta@gmail.com>
2026-06-18 22:43:35 +0200

    Update nautilus-admin-custom.py

--- nautilus-admin-custom.py
+++ nautilus-admin-custom.py
@@ -8,6 +8,15 @@ class AdminExtension(GObject.GObject, Nautilus.MenuProvider):
     def get_file_items(self, files):
         items = []
 
+        # Option: Open as administrator
+        item_open = Nautilus.MenuItem(
+            name='AdminOpen',
+            label='Open as Administrator',
+            tip='Open this file with administrator permissions'
+        )
+        item_open.connect('activate', self.open_as_admin, files)
+        items.append(item_open)
+
         # Option: Delete as administrator
         item_delete = Nautilus.MenuItem(
             name='AdminDelete',
@@ -21,6 +30,12 @@ class AdminExtension(GObject.GObject, Nautilus.MenuProvider):
 
     def get_background_items(self, current_folder):
         return []
+
+    def open_as_admin(self, menu, files):
+        for file in files:
+            path = file.get_location().get_path()
+            subprocess.Popen(['/usr/local/bin/nautilus-admin-open.sh', path])
+            break
 
     def delete_as_admin(self, menu, files):
         for file in files:
